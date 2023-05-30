ec5db568f4d8639f987780655ac04d43b048b2bb
sahin-dev <sahinsiraj360@gmail.com>
2023-05-30 08:53:14 +0600

    Differentiate with and without encoading type

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
-
+//with encoadig 'utf-8' 
+//return text
 let txt =fs.readFileSync('./input.txt','utf-8')
+//without encoding type
+//return buffer
+let buf =fs.readFileSync('./input.txt',)
 console.log(txt)
