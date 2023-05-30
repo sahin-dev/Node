9f398ab33951409c2b13034de719d42f8cf5d7a8
sahin-dev <sahinsiraj360@gmail.com>
2023-05-30 08:39:28 +0600

    Remove fs functions from master branch to create separate fs branch

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs")
-const StringDecoder = require("string_decoder").StringDecoder
-let dir = fs.readdirSync("./views")
-var buf = fs.readFileSync("./views/index.html")
-
-fs.writeFile("./input.txt","Sahin Siraj",(err)=>{
-    if(err) console.log(err)
-})
